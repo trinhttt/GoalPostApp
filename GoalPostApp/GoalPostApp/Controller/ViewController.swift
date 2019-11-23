9bcef3b9e02e1088784f43535212248bb02fac39
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2019-11-23 08:48:09 +0700

    Create app

---
+++ GoalPostApp/GoalPostApp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoalPostApp
+//
+//  Created by Trinh Thai on 11/22/19.
+//  Copyright © 2019 Trinh Thai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
